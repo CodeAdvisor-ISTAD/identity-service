91b19cd8fed467afa82543717f7bb0e4534115ba
MengseuThoeng <mengseu2004@gmail.com>
2024-12-24 08:43:44 +0700

    Add Configuration to Config-Server

--- src/test/java/co/istad/identityservice/IdentityServiceApplicationTests.java
+++ src/test/java/co/istad/identityservice/IdentityServiceApplicationTests.java
@@ -3,11 +3,11 @@ package co.istad.identityservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IdentityServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class IdentityServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
