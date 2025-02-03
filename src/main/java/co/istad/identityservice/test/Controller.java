3c6d7c18dd450073bcacd676757fd2252226351c
MengseuThoeng <mengseu2004@gmail.com>
2025-02-03 11:17:25 +0700

    test controller

--- src/main/java/co/istad/identityservice/test/Controller.java
+++ src/main/java/co/istad/identityservice/test/Controller.java
@@ -3,9 +3,11 @@ package co.istad.identityservice.test;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class Controller {
     @GetMapping("/token")
     public String getToken(Authentication authentication) {
