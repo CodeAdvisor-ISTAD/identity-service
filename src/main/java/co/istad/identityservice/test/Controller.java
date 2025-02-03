00eb8d413f89031b376cb63027f723d5ae45696d
MengseuThoeng <mengseu2004@gmail.com>
2025-02-03 10:58:03 +0700

    add test controller

---
+++ src/main/java/co/istad/identityservice/test/Controller.java
@@ -0,0 +1,21 @@
+package co.istad.identityservice.test;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
+public class Controller {
+    @GetMapping("/token")
+    public String getToken(Authentication authentication) {
+        // Check if the authentication is a valid OAuth2 token
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            // You can access the token directly from the authentication object
+            return jwtToken.getToken().getTokenValue(); // Return the access token value
+        }
+        return "No valid token found";
+    }
+
+}
